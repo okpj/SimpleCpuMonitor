d602a903a3f25fed9ccd1a4d856f4db7525ff9af
Олег Калинин <okalinin@logrocon.com>
2019-09-11 10:30:32 +0300

    Изменил параметры запуска

--- SimpleCpuMonitor/Program.cs
+++ SimpleCpuMonitor/Program.cs
@@ -13,6 +13,7 @@ namespace SimpleCpuMonitor
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+             .UseUrls("http://localhost:5000/");
     }
 }
